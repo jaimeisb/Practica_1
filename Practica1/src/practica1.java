860ff736fc51ad7e0daa9990ad53e2bd8b93253b
jaimeisb <jaimeisb2@gmail.com>
2015-03-13 20:30:38 -0600

    3
    solo faltan 4

--- Practica1/src/practica1.java
+++ Practica1/src/practica1.java
@@ -82,20 +82,13 @@ public static void promedio(){
 }
 
 public static void hora(){
-
-	/*String c;
-	int a=0,b=0;
+		String a;
+		String []hora= new String[4];
 	System.out.println("Ingrese hora");
 	Scanner s=new Scanner(System.in);
-	c=s.nextLine();
-	a=Integer.parseInt( 
-			c.substring(0, c.indexOf(":"))); 
-			b=Integer.parseInt( 
-			c.substring 
-			(c.indexOf(":")+1,c.length() ));
-	int a,b;
-	System.out.println("Ingrese hora");
-	Scanner s=new Scanner(System.in);*/
+	hora[0]=s.nextLine();
+	System.out.println(hora);
+	
 }
 
 public static void figuras(){
@@ -116,6 +109,10 @@ public static void figuras(){
 				System.out.println("El tamano del lado es:"+(a)+" el area es:"+(a*a)+" El perimetro es:"+(4*a));
 					
 			
+}
+public static void mes(){
+	
+	
 }
 public static void calculadora(){
 	int a,b;
@@ -148,7 +145,241 @@ public static void tabla(){
 			System.out.println(numero+"x"+a+"="+resultado);
 		}
 		System.out.println();
+	}
+
+public static void rango(){
+	int a,b;
+	System.out.println("Ingrese limite inferior");
+	Scanner s=new Scanner (System.in);
+		a=s.nextInt();
+	System.out.println("Ingrese limite superior");
+		b=s.nextInt();
+		int x = (int) (Math.random() *(b-a+1)+a); 
+		if(x<30){
+		switch(x){
+			case 1: System.out.print("uno"); break;
+			case 2: System.out.print("dos");break;
+			case 3: System.out.print("tres");break;
+			case 4: System.out.print("cuatro");break;
+			case 5: System.out.print("cinco");break;
+			case 6: System.out.print("seis");break;
+			case 7: System.out.print("siete");break;
+			case 8: System.out.print("ocho");break;
+			case 9: System.out.print("nueve");break;
+			case 10: System.out.print("diez");break;
+			case 11: System.out.print("once");break;
+			case 12: System.out.print("doce");break;
+			case 13: System.out.print("trece");break;
+			case 14: System.out.print("catorce");break;
+			case 15: System.out.print("quince");break;
+			case 16: System.out.print("dieciseis");break;
+			case 17: System.out.print("diesisiete");break;
+			case 18: System.out.print("diesiocho");break;
+			case 19: System.out.print("diesinueve");break;
+			case 20: System.out.print("veinte");break;
+			case 21: System.out.print("veintiuno");break;
+			case 22: System.out.print("veintidos");break;
+			case 23: System.out.print("veintitres");break;
+			case 24: System.out.print("veinticuatro");break;
+			case 25: System.out.print("veinticinco");break;
+			case 26: System.out.print("veintiseis");break;
+			case 27: System.out.print("veintisiete");break;
+			case 28: System.out.print("veintiocho");break;
+			case 29: System.out.print("veintinueve");break;
+			
+		}
+		}
+		else{
+		
+		switch((x/100)%10){
+			case 1: System.out.print("cien"); break;
+		}
+		switch((x/10)%10){
+			
+			case 2: System.out.print("veinti");break;
+			case 3: System.out.print("treinta");break;
+			case 4: System.out.print("cuarenta");break;
+			case 5: System.out.print("cincuenta");break;
+			case 6: System.out.print("sesenta");break;
+			case 7: System.out.print("setenta");break;
+			case 8: System.out.print("ochenta");break;
+			case 9: System.out.print("noventa");break;
+		}
+		switch(x%10){
+			case 1: System.out.print(" y uno"); break;
+			case 2: System.out.print(" y dos");break;
+			case 3: System.out.print(" y tres");break;
+			case 4: System.out.print(" y cuatro");break;
+			case 5: System.out.print(" y cinco");break;
+			case 6: System.out.print(" y seis");break;
+			case 7: System.out.print(" y siete");break;
+			case 8: System.out.print(" y ocho");break;
+			case 9: System.out.print(" y nueve");break;
+			
+		}
+		
+		}
+		System.out.println(" = "+x);
 }
+
+
+public static void conversion(){
+	int n;
+	System.out.println("Ingrese el numero que desea convertir");
+	Scanner s=new Scanner (System.in);
+	n=s.nextInt();
+	switch((n/1000)%10){
+		case 1: System.out.print("M"); break;
+		case 2: System.out.print("MM");break;
+		case 3: System.out.print("MMM");break;
+		case 4: System.out.print("__\nIV");break;
+		case 5: System.out.print("_\nV");break;
+		case 6: System.out.print("__\nVI");break;
+		case 7: System.out.print("___\nVII");break;
+		case 8: System.out.print("____\nVIII");break;
+		case 9: System.out.print("__\nIX");break;
+	}
+	switch((n/100)%10){
+		case 1: System.out.print("C");break;
+		case 2: System.out.print("CC");break;
+		case 3: System.out.print("CCC");break;
+		case 4: System.out.print("CD");break;
+		case 5: System.out.print("D");break;
+		case 6: System.out.print("DC");break;
+		case 7: System.out.print("DCC");break;
+		case 8: System.out.print("DCCC");break;
+		case 9: System.out.print("CM");break;
+	}
+	switch((n/10)%10){
+		case 1: System.out.print("x");break;
+		case 2: System.out.print("XX");break;
+		case 3: System.out.print("XXX");break;
+		case 4: System.out.print("XL");break;
+		case 5: System.out.print("L");break;
+		case 6: System.out.print("LX");break;
+		case 7: System.out.print("LXX");break;
+		case 8: System.out.print("LXXX");break;
+		case 9: System.out.print("XC");break;
+	}
+	switch(n%10){
+		case 1: System.out.print("I");break;
+		case 2: System.out.print("II");break;
+		case 3: System.out.print("III");break;
+		case 4: System.out.print("IV");break;
+		case 5: System.out.print("V");break;
+		case 6: System.out.print("VI");break;
+		case 7: System.out.print("VII");break;
+		case 8: System.out.print("VIII");break;
+		case 9: System.out.print("IX");break;
+	}
+	System.out.println(" = "+n);
+}
+
+public static void piramide(){
+	 int n = 0,a=1,b=0;
+     Scanner s=new Scanner(System.in);
+     System.out.println("Ingrese un numero");
+     n=s.nextInt();
+	     for (int i=n;i>0;i--){
+	    	 for(int j=n-i;j>0;j--)
+	    		 System.out.print(" ");
+	    	 for( int k=i-1;k>0;k--)			
+	    		 System.out.print(a);
+    	 System.out.println(a++);
+      }
+     /*for(i=9;i>1;i--){
+	if((i%2)==1){
+		for(k=i;k<=11;k++){
+			if((k%2)==1){
+				System.out.print(" ");
+			}
+		}
+		for(j=i-2;j>0;j--){
+			System.out.print("*");
+		}
+		System.out.println();
+		for(int j=n-i;j>0;j--)*/
+     
+      
+}
+public static void cadena(){
+
+	        String palabra;
+	        Scanner s=new Scanner(System.in);
+	        System.out.println("Ingrese una palabra");
+	        palabra=s.nextLine();
+	       
+	        	String[] asteriscos = palabra.split("");
+	        	for(int i = 0; i < asteriscos.length; i++) 
+	        		System.out.print("*");
+}
+	  
+public static void cuadrado(){
+	int n;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Ingrese un numero");
+    n=s.nextInt();
+    for(int i=1;i<=n;i++){
+    	for(int j=1;j<=n;j++){
+    		if(j==1||j==n||i==1||i==n){
+    			System.out.print("* ");
+    		}
+    			else{
+    				System.out.print("  ");
+    			}
+        }
+    	System.out.println();
+    }
+}
+
+public static void juego(){
+	String abc="abcdefghijklmnopqrstuvwxyz";
+	char m;
+	char k;
+	Scanner s=new Scanner(System.in);
+	System.out.println("Ingrese la letra a buscar");
+	m=s.next().charAt(0);
+	for(int i=0; i < abc.length(); i++ ){
+		if( abc.charAt(i)== m){
+			//System.out.println("La posicion de la palabra es " + i);
+			int z=i;
+		
+	System.out.println("Ingrese la letra que creas que es");
+	k=s.next().charAt(0);
+	for(int j=0; j < abc.length(); j++ ){
+		if( abc.charAt(j)== k){
+			//System.out.println("La posicion de la palabra es " + j);
+			int y=j;
+			do {
+			if(y<z)
+				System.out.println ("despues" );
+			k=s.next().charAt(0);
+			for( j=0; j < abc.length(); j++ ){
+				if( abc.charAt(j)== k){
+					//System.out.println("La posicion de la palabra es " + j);
+					 y=j;}}
+        	if(y>z)
+        		System.out.println ("antes" );
+          	k=s.next().charAt(0);
+        	for( j=0; j < abc.length(); j++ ){
+        		if( abc.charAt(j)== k){
+        			//System.out.println("La posicion de la palabra es " + j);
+        			 y=j;}}}
+								
+			while(y!=z);
+			
+				   
+		}   
+	}
+		}
+	
+	}
+	
+}
+
+
+
+
 public static void main(String[] args) {
 	menu();
 	int num=0;
@@ -159,7 +390,14 @@ public static void main(String[] args) {
 		case 2: promedio();break;
 		case 3: hora();break;
 		case 4: figuras();break;
+		case 5: mes();break;
 		case 6: calculadora();break;
 		case 7: tabla();break;
+		case 8: rango();break;
+		case 9: conversion();break;
+		case 10: piramide();break;
+		case 12: cadena();break;
+		case 13: juego();break;
+		case 14: cuadrado();break;
 		}
 }}
